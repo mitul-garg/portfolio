c469e104c49713e2fafad4caa179d1680471735f
unknown <mitulgarg2000@gmail.com>
2021-07-07 22:19:45 +0530

    About Page Completed

---
+++ src/data/skills.js
@@ -0,0 +1,26 @@
+export const skills = [
+  {
+    id: 1,
+    name: "java",
+  },
+  {
+    id: 2,
+    name: "react.js",
+  },
+  {
+    id: 3,
+    name: "html",
+  },
+  {
+    id: 4,
+    name: "css",
+  },
+  {
+    id: 5,
+    name: "javascript",
+  },
+  {
+    id: 6,
+    name: "firebase",
+  },
+];
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,32 @@
 import React from "react";
+import heroImgAbout from "../assets/images/hero.svg";
+import { skills } from "../data/skills";
 
 export const About = () => {
-  return <div className="about">about</div>;
+  return (
+    <div className="about">
+      <img src={heroImgAbout} alt="hero" className="about-hero" />
+      <article className="about-info">
+        <h1>About Me</h1>
+        <div className="about-para">
+          I am Mitul Garg, an undergoing Computer Science Undergrad at Shri
+          Govindram Seksaria Institute of Technology and Science, Indore. Since
+          my school days, I have been quite passionate about computers and have
+          always wanted to work on them. Talking about my hobbies and interests,
+          I love playing and watching Cricket and Chess matches. At present, I
+          am seeking an Internship to apply and improve on honed skills.
+        </div>
+        <div className="skills">
+          {skills.map((skill) => {
+            const { id, name } = skill;
+            return (
+              <button key={id} className="skill">
+                {name}
+              </button>
+            );
+          })}
+        </div>
+      </article>
+    </div>
+  );
 };
