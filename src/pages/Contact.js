8c49d0f15f7483a4bd44aceb618b2de0627a36bb
unknown <mitulgarg2000@gmail.com>
2021-07-08 08:45:57 +0530

    Contact Page Completed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,44 @@
 import React from "react";
 
 export const Contact = () => {
-  return <div className="contact">contact</div>;
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  // };
+
+  return (
+    <div className="contact">
+      <article className="contact-form">
+        <h3>Get In Touch</h3>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="form-group">
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              className="form-control"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="form-control"
+              required
+            />
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="message"
+              className="form-control"
+              required
+            />
+          </div>
+          <button type="submit" className="submit-btn">
+            submit here
+          </button>
+        </form>
+      </article>
+    </div>
+  );
 };
