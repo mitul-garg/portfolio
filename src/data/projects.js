fad322e7b17b71d9278149f14f8428d25c8669e9
unknown <mitulgarg2000@gmail.com>
2021-10-18 21:14:37 +0530

    Optimised Images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,12 @@
-import img1 from "../assets/images/projects-1.jpg";
-import img2 from "../assets/images/projects-2.jpg";
-import img3 from "../assets/images/projects-3.jpg";
+import img1 from "../assets/projects-1.jpg";
+import img2 from "../assets/projects-2.jpg";
+import img3 from "../assets/projects-3.jpg";
 
 export const projects = [
   {
     id: 0,
     title: "Location Blog",
-    img: img3,
+    img: img1,
     details:
       "Location Blog is a MERN App where everyone can share images of the places they visit and also see other people's places.",
     technologies: ["MERN Stack"],
@@ -16,7 +16,7 @@ export const projects = [
   {
     id: 1,
     title: "Portfolio",
-    img: img1,
+    img: img3,
     details:
       "A Single Page React App built with react-router-dom for navigation on other pages.The Portfolio is completely responsive and looks good on all screen sizes. All the data is dynamically loaded in each component using Javascript imports!",
     technologies: ["CSS", "Javascript", "React JS"],
@@ -36,7 +36,7 @@ export const projects = [
   {
     id: 3,
     title: "TODO Sync App",
-    img: img3,
+    img: img1,
     details:
       "Login Sign-up system using Email and Password authentication provided by Firebase. The app is completely responsive and syncs the todos to the Firestore. Todos can be added, edited and deleted in single clicks",
     technologies: ["HTML", "CSS", "Javascript", "Firebase"],
@@ -46,7 +46,7 @@ export const projects = [
   {
     id: 4,
     title: "Kalaamanch Website",
-    img: img1,
+    img: img3,
     details:
       "A basic landing page for our Organization with all our details. All our competitions are organised smoothly on this website",
     technologies: ["HTML", "CSS", "Javascript"],
@@ -66,7 +66,7 @@ export const projects = [
   {
     id: 6,
     title: "Mini Web Projects",
-    img: img3,
+    img: img1,
     details:
       "These projects were made by me to practice Vanilla JS. The ideas were taken from various courses provided by Edyoda and Freecodecamp",
     technologies: ["HTML", "CSS", "Javascript"],
