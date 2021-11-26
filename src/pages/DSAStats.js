255aead529dbf932d93a73ae277e39aed8d98b90
unknown <mitulgarg2000@gmail.com>
2021-11-26 08:16:25 +0530

    Updated DSA Stats Page

--- src/pages/DSAStats.js
+++ src/pages/DSAStats.js
@@ -2,16 +2,18 @@ import React from "react";
 
 import { Doughnut } from "react-chartjs-2";
 
+import { SiLeetcode, SiCodechef, SiGeeksforgeeks } from "react-icons/si";
+
 const problemsData = {
-  labels: ["GFG Practice", "Codechef", "Codeforces", "Leetcode"],
+  labels: ["GFG Practice", "Codechef", /*"Codeforces" ,*/ "Leetcode"],
   datasets: [
     {
-      data: [396, 60, 298, 88],
-      backgroundColor: ["#2a9d8f", "#cb997e", "#264653", "#e9c46a"],
+      data: [397, 72, /*298,*/ 139],
+      backgroundColor: ["#2a9d8f", "#cb997e", /*"#264653",*/ "#e9c46a"],
       hoverBackgroundColor: [
         "#2a9d8fdd",
         "#cb997edd",
-        "#264653dd",
+        // "#264653dd",
         "#e9c46add",
       ],
     },
@@ -22,7 +24,7 @@ export const DSAStats = () => {
   return (
     <div className="dsa">
       <div className="charts-container">
-        <h3 className="opensource-title">Problems Solved : 800+</h3>
+        <h3 className="opensource-title">Problems Solved : 600+</h3>
         <div className="opensource-charts">
           <div className="chart">
             <h3>Distribution</h3>
@@ -33,18 +35,22 @@ export const DSAStats = () => {
       <div className="dsastats-details skills">
         <button className="dsa-button skill">
           <a href="https://auth.geeksforgeeks.org/user/mitulgarg/practice/">
-            GFG Profile
+            GFG Profile <SiGeeksforgeeks />
           </a>
         </button>
         <button className="dsa-button skill">
-          <a href="https://leetcode.com/mitulgarg/">Leetcode</a>
+          <a href="https://leetcode.com/mitulgarg/">
+            Leetcode <SiLeetcode />{" "}
+          </a>
         </button>
         <button className="dsa-button skill">
-          <a href="https://www.codechef.com/users/mitulgarg">Codechef</a>
+          <a href="https://www.codechef.com/users/mitulgarg">
+            Codechef <SiCodechef />
+          </a>
         </button>
-        <button className="dsa-button skill">
-          <a href="https://codeforces.com/profile/mitulgarg">Codeforces</a>
-        </button>
+        {/* <button className="dsa-button skill">
+          <a href="https://codeforces.com/profile/mitul-garg">Codeforces</a>
+        </button> */}
       </div>
     </div>
   );
