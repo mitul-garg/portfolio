d6438fe5ab860ac38079b1f8fec63c7d24bbf532
mitul-garg <mitulgarg2000@gmail.com>
2024-06-19 12:29:31 +0530

    Fixed Hero CSS

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,19 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { socialLinks } from "../../data/socialLinks";
 import heroImg from "../../assets/images/hero.svg";
-import { ResumeButton } from "../CVButton";
+import { MdFileDownload } from "react-icons/md";
+import "./styles.css";
 
 export const Hero = () => {
   return (
     <section className="hero">
       <article className="intro">
-        <div className="underline"></div>
+        <div className="underline-desktop" />
         <h1>I'm Mitul</h1>
         <h6>Software Engineer @ Optum</h6>
-        <ResumeButton />
-        <Link to="/contact">
-          <button className="hero-btn">Contact Me</button>
-        </Link>
+        <div className="underline-mobile" />
+        <img src={heroImg} alt="hero" className="hero-img-mobile" />
+        <div className="hero-btn-container">
+          <button className="hero-btn resume">
+            <a
+              href="https://drive.google.com/file/d/1-4YBRo8oqRfJbIvyQoY8g7l2f4OAxHWI/view"
+              className="resume-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume <MdFileDownload className="resume-download-icon" />
+            </a>
+          </button>
+          <Link to="/contact">
+            <button className="hero-btn">Contact Me</button>
+          </Link>
+        </div>
         <ul className="hero-social-links">
           {socialLinks.map((link) => {
             const { id, icon, url } = link;
@@ -33,7 +47,7 @@ export const Hero = () => {
           })}
         </ul>
       </article>
-      <img src={heroImg} alt="hero" className="hero-img" />
+      <img src={heroImg} alt="hero" className="hero-img-desktop" />
     </section>
   );
 };
